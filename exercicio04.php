08f3b0c71df0cb0660cc8052f1746d5c4580ba2d
Guisoares_46 <guiguismiranda@outlook.com>
2025-03-31 20:19:35 -0300

    atualizar exercício 4

--- exercicio04.php
+++ exercicio04.php
@@ -1,23 +1,30 @@
 <?php
 	
-	$n1 = 1;
-	$n2 = 11;
-	$n3 = 999;
+	$n1 = 46;
+	$n2 = 10;
+	$n3 = 4;
 
-	/*if (($n1 > $n2 )&&($n1 > $n3)&&($n2 > $n3)){
+	if ($n1 > $n3 && $n1 > $n2){
+		if ($n2 > $n3){
 		echo "$n1 <br> $n2 <br> $n3";
-	}elseif(($n2 > $n1)&&($n2 > $n3)){
-		echo "$n2 <br> $n1 <br> $n3";
 	}else{
-		echo "$n3 <br> $n2 <br> $n1";
-	}*/
-	for($i = 0; $i < 4; $i++){
-		if($n1 > $n2){
-			echo "$n1 $n2<br>";
+		echo "$n1 <br> $n3 <br> $n2";
 		}
-		if($n1 > $n2 && $n2 > $n3){
-			echo "oi";
+	}else{
+	if ($n2 > $n1 && $n2 > $n3){
+		if($n1 > $n3){
+			echo "$n2 <br> $n1 <br> $n3";
+		}else{
+			echo "$n2 <br> $n3 <br> $n1";
+	}
+	}
+	if($n3 > $n1 && $n3 > $n2){
+		if($n2 > $n1){
+		}else{
+			echo "$n3 <br> $n1 <br> $n3";
 		}
+	}
+
 	}
 
 ?>
