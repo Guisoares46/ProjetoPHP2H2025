e4b386c131ccb810a0e9227a625371e5154095fc
Guisoares46 <guiguismiranda@outlook.com>
2025-04-07 20:08:21 -0300

    adicionando mais conteúdo

---
+++ funcao.php
@@ -0,0 +1,17 @@
+<?php
+
+//criando uma função
+function somaValor(){
+	$num1 = 10;
+	$num2 = 20;
+
+	$resp = $num1 + $num2;
+
+	echo "A soma de num1 + num2 é: " . $resp;
+}
+
+//executando uma função
+	somaValor();
+
+
+?>
---
+++ funcaon2.php
@@ -0,0 +1,15 @@
+<?php
+
+$num1 = 10;
+$num2 = 20;
+
+//criando uma função
+function somaValor($v1,$v2){
+
+	return ($v1 + $v2);
+}
+
+//executando uma função
+	echo "A soma de num1 + num2 é: " . somaValor($num1, $num2);
+
+?>
